eb878a14db30aaa35923140788b783efe36d1c47
Joseph Obiagba <j.i.obiagba2016@gmail.com>
2019-05-31 02:20:48 +0100

    mongodb updated

--- app.js
+++ app.js
@@ -18,8 +18,7 @@ var app = express();
 
 //Set up mongoose
 var mongoose = require('mongoose');
-var dev_db_url = 'mongodb+srv://jiobiagbaDB:obidudu9293@cluster0-ywgnl.azure.mongodb.net/local_library?retryWrites=true';
-var mongoDB = process.env.MONGODR_URI || dev_db_url;
+var mongoDB = process.env.MONGODR_URI
 mongoose.connect(mongoDB, {useNewUrlParser: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
