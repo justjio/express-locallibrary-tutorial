dfc613e4b88f87b46d660e2d5e4947847191a64b
Joseph Obiagba <j.i.obiagba2016@gmail.com>
2019-06-02 14:42:03 +0100

    email field of user model updated to the SchemaTypes.

--- models/user.js
+++ models/user.js
@@ -1,13 +1,15 @@
 var moment = require('moment');
 var mongoose = require('mongoose');
+require('mongoose-type-email');
 
 var Schema = mongoose.Schema;
+
 
 var UserSchema = new Schema(
     {
     first_name: {type: String, required: true, max: 100},
     last_name: {type: String, required: true, max: 100},
-    email: {type: String, required: true, max: 100},
+    email: {type: mongoose.SchemaTypes.Email, required: true},
     gender: {type: String, required: true, enum: ['Male', 'Female'], default: 'Male'},
     date_of_birth: {type: Date},
     password: {type: String, required: true, max: 100}
@@ -20,6 +22,10 @@ UserSchema
     return this.first_name + ' ' + this.last_name;
 });
 
-UserSchema.virtual('age').get(function () {
+UserSchema
+.virtual('age')
+.get(function () {
     return moment().year() - moment(this.date_of_birth).year;
-})
+});
+
+module.exports = mongoose.model('User', UserSchema);
