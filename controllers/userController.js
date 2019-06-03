1748a2a1072706827c1fed86047e92026785383f
Joseph Obiagba <j.i.obiagba2016@gmail.com>
2019-06-03 15:28:20 +0100

    To account for signup and login before access, appropriate routes have been added and tested along side necessary controllers.

---
+++ controllers/userController.js
@@ -0,0 +1,21 @@
+var User = require('../models/user');
+
+//Display SignUp form on GET
+exports.user_signup_get = function(req, res, next) {
+    res.send('Sign Up Page GET Under Construction!')
+};
+
+//Handle SignUp form on POST
+exports.user_signup_post = function(req, res, next) {
+    res.send('Sign Up Page POST Under Construction!')
+};
+
+//Display Login form on GET
+exports.user_login_get = function(req, res, next) {
+    res.send('Login Page GET Under Construction!')
+};
+
+//Handle Login form on POST
+exports.user_login_post = function(req, res, next) {
+    res.send('Login Page POST Under Construction!')
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+var user_controller = require('../controllers/userController');
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+//router.get('/', function(req, res, next) {
+//  res.send('respond with a resource');
+//});
+
+//GET Signup Page
+router.get('/signup', user_controller.user_signup_get);
+
+//POST Signup Page
+router.post('/signup', user_controller.user_signup_post);
+
+//GET Login Page
+router.get('/login', user_controller.user_login_get);
+
+//POST Login Page
+router.post('/login', user_controller.user_login_post);
 
 module.exports = router;
